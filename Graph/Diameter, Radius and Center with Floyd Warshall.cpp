2fef09aadc8c22643fb3bf88eee5cd2905a9c544
Tiago Silva <47832651+PDSTiago@users.noreply.github.com>
2020-09-23 00:47:47 -0300

    Create Diameter, Radius and Center with Floyd Warshall.cpp

---
+++ Graph/Diameter, Radius and Center with Floyd Warshall.cpp
@@ -0,0 +1,34 @@
+    int n, dist[mxn][mxn], excen[mxn];
+
+    int diam=-1, radius=INF;
+
+    vector<int>center;
+
+    memset(dist, INF, sizeof(dist));
+
+    memset(excen, -1, sizeof(excen));
+
+    for(int v=0; v<n; v++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++) {
+                dist[i][j]=min(dist[i][j], dist[i][v]+dist[v][j]);
+            }
+        }
+    }
+    
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            excen[i]=max(excen[i], dist[i][j]);
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        radius=min(radius, excen[i]);
+        diam=max(diam, excen[i]);
+    }
+
+    for(int i=0; i<n; i++){
+        if(radius==excen[i]){
+            center.push_back(i);
+        }
+    }
