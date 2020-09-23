662fc3ebdd7d9a0d70ca5c084cdf423949a763c8
Tiago Silva <47832651+PDSTiago@users.noreply.github.com>
2020-09-23 00:50:14 -0300

    Update Diameter, Radius and Center.cpp

--- Graph/Diameter, Radius and Center.cpp
+++ Graph/Diameter, Radius and Center.cpp
@@ -4,74 +4,41 @@ vector<int> tree[mxn];
 void bfs(int x){
 	memset(memo, 0, sizeof (memo)); 
 	memset(dist, 0, sizeof (dist));
-
 	queue<int> fila;
 	memo[x]=1;
-
 	fila.push(x);
-
 	while(!fila.empty()){
-
 		int no=fila.front();
 		fila.pop();
-
 		for(int i=0; i<tree[no].size() ; i++){
 			int v=tree[no][i];
 			if(memo[v]==0){
-
 				dist[v]=dist[no]+1;
-
 				memo[v]=1;
-
 				pai[v]=no;
-
 				fila.push(v);
 			}
 		}
 	}
 }
-int main(){
-    cin >> n;
-    for(int i=1; i<n; i++){
-        int a, b;
-        cin >> a >> b;
-        tree[a].push_back(b);
-        tree[b].push_back(a);
-    }
-
-    bfs(1);
-
-    int maior=-1, pos;
-
-    for(int i=1; i<=n; i++){
-        if(dist[i]>maior){
-            maior=dist[i];
-            pos=i;
-        }
-    }
-
-    bfs(pos);
-
-    int diametro=-1, raio, centro, atual;
-
-    for(int i=1; i<=n; i++){
-        if(dist[i]>diametro){
-            diametro=dist[i];
-            atual=i;
-        }
-    }
-
-    raio=(diametro+1)/2;
-
-    while(dist[atual]>diametro/2){
-        atual=pai[atual];
-    }
-
-    centro=atual;
-
-    cout << diametro << endl;
-    cout << raio << endl;
-    cout << centro << endl;
-
-	return 0;
+bfs(1);
+int maior=-1, pos;
+for(int i=1; i<=n; i++){
+	if(dist[i]>maior){
+	    maior=dist[i];
+	    pos=i;
+	}
 }
+bfs(pos);
+int diametro=-1, raio, centro, atual;
+for(int i=1; i<=n; i++){
+	if(dist[i]>diametro){
+		diametro=dist[i];
+		atual=i;
+	}
+}
+raio=(diametro+1)/2;
+while(dist[atual]>diametro/2){
+	atual=pai[atual];
+}
+centro=atual;
