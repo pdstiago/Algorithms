4d451ca02bfaed006362693c0b87a3254d813acd
Tiago Silva <47832651+pdstiago@users.noreply.github.com>
2022-05-03 12:45:08 -0300

    Create Header.cpp

---
+++ Header.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000009
+#define mxn 100005
+#define mxm 505
+#define f first
+#define s second
+#define pb push_back
+#define es " "
+#define endl '\n'
+#define INF 0x3f3f3f3f
+#define INFL 0x3f3f3f3f3f3f3f3f
+#define ll long long
+#define fastio ios_base::sync_with_stdio(0), cin.tie(0)
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+typedef pair<string, int> pii;
+typedef pair<int, pii> pip;
+
+int main(){
+    fastio;
+    
+    return 0;
+}
