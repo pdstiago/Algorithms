4be8ef5a265b29166972627f323197a466b1209e
Tiago Silva <tiago.pds.silva@gmail.com>
2022-10-03 20:19:52 -0300

    Update Lazy.cpp

--- Data Structures/Lazy.cpp
+++ Data Structures/Lazy.cpp
@@ -1,44 +1,44 @@
 void unlazy(int node, int l, int r){
-    if(tree[node][1]){
-        tree[node][0]=tree[node][1];
+    if(lazy[node]){
+        tree[node]=(r-l+1)*lazy[node];
         if(l!=r){
-            tree[2*node][1]=tree[node][1];
-            tree[2*node+1][1]=tree[node][1];
+            lazy[2*node]=lazy[node];
+            lazy[2*node+1]=lazy[node];
         }
-        tree[node][1]=0;
+        lazy[node]=0;
     }
 }
 
 void build(int node, int l, int r){
     if(l==r){
-        tree[node][0]=v[l];
+        tree[node]=v[l];
         return;
     }
     int mid=(l+r)>>1;
     build(2*node, l, mid);
     build(2*node+1, mid+1, r);
-    tree[node][0]=tree[2*node][0]+tree[2*node+1][0];
+    tree[node]=tree[2*node]+tree[2*node+1];
 }
 
-void update(int node, int l, int r, int a, int b, int val){
+void upd(int node, int l, int r, int a, int b, int val){
     unlazy(node, l, r);
     if(a>r || b<l) return;
     if(l>=a && r<=b){
-        tree[node][1]=val;
+        lazy[node]=val;
         unlazy(node, l, r);
         return;
     }
     int mid=(l+r)>>1;
-    update(2*node, l, mid, a, b);
-    update(2*node+1, mid+1, r, a, b);
-    tree[node][0]=tree[2*node][0]+tree[2*node+1][0];
+    upd(2*node, l, mid, a, b, val);
+    upd(2*node+1, mid+1, r, a, b, val);
+    tree[node]=tree[2*node]+tree[2*node+1];
 }
 
 int query(int node, int l, int r, int a, int b){
     unlazy(node, l, r);
     if(a>r || b<l) return 0;
     if(a<=l && b>=r){
-        return tree[node][0];
+        return tree[node];
     }
     int mid=(l+r)>>1;
     return query(2*node, l, mid, a, b)+query(2*node+1, mid+1, r, a, b);
