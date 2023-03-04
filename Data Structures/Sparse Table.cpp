655169832a0b82f14b85c6677e7e22a5f2ea8c97
Tiago Silva <tiago.pds.silva@gmail.com>
2023-03-04 03:05:57 -0300

    Create Sparse Table.cpp

---
+++ Data Structures/Sparse Table.cpp
@@ -0,0 +1,35 @@
+void computa() {
+    for(int i=1; i<=n; i++) tab[i][0]=v[i];
+    for(int j=1; j<=10; j++) {
+        for(int i=1; i<=n; i++) {
+            if(i + (1<<j)-1 > n) break;
+            tab[i][j]=tab[i][j-1] + 
+            tab[i+(1<<(j-1))][j-1]; 
+            //min or max after =
+        }
+    }
+}
+
+int flog(int x) {
+    return 31 - __builtin_clz(x);
+}
+
+int flog(long long x) {
+    return 63 - __builtin_clzll(x);
+}
+
+int queryminmax(int l, int r) {
+    int k = flog(r-l+1);
+    return min(tab[l][k], tab[r-(1<<k)+1][k]);
+}
+
+int querysum(int l, int r){
+    int ans=0;
+    for(int i=10; i>=0; i--){
+        if(l + (1<<i) - 1 <= r){
+            ans+=tab[l][i];
+            l+=(1<<i);
+        }
+    }
+    return ans;
+}
