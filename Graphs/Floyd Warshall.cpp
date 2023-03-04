de44e14881beeae8307e20e8a6e7b9118ec1ffbd
Tiago Silva <tiago.pds.silva@gmail.com>
2023-03-04 03:22:15 -0300

    Update and rename FloydWarshall.cpp to Floyd Warshall.cpp

---
+++ Graphs/Floyd Warshall.cpp
@@ -0,0 +1,28 @@
+void construct(){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if(i==j){
+                dist[i][j]=0;
+                continue;
+            }
+            dist[i][j]=INF;
+        }
+    }   
+}
+void input(){
+    for(int i=1; i<=m; i++){
+        int a, b, c;
+        cin >> a >> b >> c;
+        dist[a][b]=min(dist[a][b], c);
+        dist[b][a]=min(dist[b][a], c);
+    }   
+}
+void floyd(){
+    for(int k=1; k<=n; k++){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                dist[i][j]=min(dist[i][j], dist[i][k]+dist[k][j]);
+            }
+        }
+    }   
+}
