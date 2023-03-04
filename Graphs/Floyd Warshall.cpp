356d2f3ce7a6c7212814522d2c9ec5c7124cc2e8
Tiago Silva <tiago.pds.silva@gmail.com>
2023-03-04 03:23:09 -0300

    Update Floyd Warshall.cpp

--- Graphs/Floyd Warshall.cpp
+++ Graphs/Floyd Warshall.cpp
@@ -1,11 +1,8 @@
 void construct(){
     for(int i=1; i<=n; i++){
         for(int j=1; j<=n; j++){
-            if(i==j){
-                dist[i][j]=0;
-                continue;
-            }
             dist[i][j]=INF;
+            if(i==j) dist[i][j]=0;
         }
     }   
 }
@@ -21,7 +18,8 @@ void floyd(){
     for(int k=1; k<=n; k++){
         for(int i=1; i<=n; i++){
             for(int j=1; j<=n; j++){
-                dist[i][j]=min(dist[i][j], dist[i][k]+dist[k][j]);
+                dist[i][j]=min(dist[i][j], 
+                dist[i][k]+dist[k][j]);
             }
         }
     }   
