190c25810fbddcfcc01f8a3cd03775392ffdcbd6
Tiago Silva <tiago.pds.silva@gmail.com>
2023-03-04 03:37:17 -0300

    Update Z Algorithm.cpp

--- Strings/Z Algorithm.cpp
+++ Strings/Z Algorithm.cpp
@@ -1,8 +1,8 @@
 //z é um vector de int que relaciona cada posição da string com um número 
 //que indica o tamanho da subsequência que começa nessa posição e coincide 
 //com o prefixo da string
-void z_algo(string s) {
-    for(int i=1, l=0, r=0; i<n; i++) {
+void z_algo(string s){
+    for(int i=1, l=0, r=0; i<n; i++){
         if(i<=r){
             z[i]=min(r-i+1, z[i-l]);
         }
