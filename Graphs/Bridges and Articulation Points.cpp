4433b32745764a8e47b8ea35b0ab2d2262747f63
Tiago Silva <tiago.pds.silva@gmail.com>
2023-03-04 03:15:59 -0300

    Update and rename Bridges_And_Points.cpp to Bridges and Articulation Points.cpp

--- Graphs/Bridges and Articulation Points.cpp
+++ Graphs/Bridges and Articulation Points.cpp
@@ -30,14 +30,4 @@ void find_bridges_and_points(){
             dfs(i, 0);
         }
     }
-    cout << endl << "Points: " << endl;
-    for(int i=1; i<=n; i++){
-        cout << artic[i] << es;
-    }
-    cout << endl << endl << "Bridges: " << endl;
-    sort(all(bridges));
-    for(pii i:bridges){
-        cout << i.f << es << i.s << endl;
-    }
-    cout << endl;
 }
