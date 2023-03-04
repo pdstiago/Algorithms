7ed8f792b1ef32e5b5c10963304131d3fa983e58
Tiago Silva <tiago.pds.silva@gmail.com>
2023-03-04 03:34:25 -0300

    Create Fast Exponentiation.cpp

---
+++ Math/Fast Exponentiation.cpp
@@ -0,0 +1,13 @@
+ll exp(ll b, ll e){
+	ll res=1;
+	while(e){
+        if(e&1){
+            res=mod(res*b, MOD);
+            res=mod(res, MOD);
+        }
+        b=mod(b*b, MOD);
+        b=mod(b, MOD);
+	    e>>=1;
+	}
+	return res;
+}
