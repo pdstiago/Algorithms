99e85e3aa781f16509d70236548161b55d7ab3e7
Tiago Silva <tiago.pds.silva@gmail.com>
2023-03-06 22:32:55 -0300

    Update Segment Tree.cpp

--- Data Structures/Segment Tree.cpp
+++ Data Structures/Segment Tree.cpp
@@ -4,23 +4,18 @@ void build(int node=1, int l=1, int r=n){
         return;
     }
     int mid=(l+r)>>1;
-    build(2*node, l, mid);
-    build(2*node+1, mid+1, r);
+    build(2*node, l, mid), build(2*node+1, mid+1, r);
     tree[node]=tree[2*node]+tree[2*node+1];
 }
 
 void upd(int id, int val, int node=1, int l=1, int r=n){
     if(l==r){
-        tree[node]=val;
-        v[l]=val;
+        tree[node]=val, v[l]=val;
         return;
     }
     int mid=(l+r)>>1;
-    if(l<=id && id<=mid){
-        upd(id, val, 2*node, l, mid);
-    }else{
-        upd(id, val, 2*node+1, mid+1, r);
-    }
+    if(l<=id && id<=mid) upd(id, val, 2*node, l, mid);
+    else upd(id, val, 2*node+1, mid+1, r);
     tree[node]=tree[2*node]+tree[2*node+1];
 }
 
@@ -28,5 +23,5 @@ int query(int a, int b, int node=1, int l=1, int r=n){
     if(b<l || a>r) return 0;
     if(l>=a && r<=b) return tree[node];
     int mid=(l+r)>>1;
-    return query(a, b, 2*node, l, mid, a, b)+query(a, b, 2*node+1, mid+1, r, a, b);
+    return query(a, b, 2*node, l, mid)+query(a, b, 2*node+1, mid+1, r);
 }
