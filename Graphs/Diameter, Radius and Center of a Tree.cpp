85b39c81802d196b102f980cef6623ac1d4f0330
Tiago Silva <tiago.pds.silva@gmail.com>
2023-03-09 02:50:40 -0300

    Update Diameter, Radius and Center of a Tree.cpp

--- Graphs/Diameter, Radius and Center of a Tree.cpp
+++ Graphs/Diameter, Radius and Center of a Tree.cpp
@@ -1,33 +1,33 @@
 void dfs(int x, int p){
     for(int i:vec[x]){
         if(i==p) continue;
-        pai[i]=x;
         dist[i]=dist[x]+1;
         dfs(i, x);
     }
 }
 void tree(){
-    int maior=0, p1, p2, diam, raio, centro, atual;
+    int p1, p2, centro, diam=-1, raio=INF;
     dfs(1, 0);
     for(int i=1; i<=n; i++){
-        if(dist[i]>maior){
-            maior=dist[i];
+        if(dist[i]>diam){
+            diam=dist[i];
             p1=i;
         }
     }
+    memset(dist, 0, sizeof(dist));
     dfs(p1, 0);
-    maior=0;
+    for(int i=1; i<=n; i++) exc[i]=dist[i];
+    diam=-1;
     for(int i=1; i<=n; i++){
-        if(dist[i]>maior){
-            maior=dist[i];
+        if(dist[i]>diam){
+            diam=dist[i];
             p2=i;
         }
     }
-    diam=maior;
-    raio=(diam+1)/2;
-    atual=p2;
-    while(dist[atual]>diam/2){
-        atual=pai[atual];
+    memset(dist, 0, sizeof(dist));
+    dfs(p2, 0);
+    for(int i=1; i<=n; i++) exc[i]=max(exc[i], dist[i]);
+    for(int i=1; i<=n; i++){
+        if(exc[i]<raio) raio=exc[i], centro=i;
     }
-    centro=atual;
 }
