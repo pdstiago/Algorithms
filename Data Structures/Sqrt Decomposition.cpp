59e22342d71d07457ee9c2136a877fea3a6e9717
Tiago Silva <tiago.pds.silva@gmail.com>
2023-03-21 03:30:17 -0300

    Create Sqrt Decomposition.cpp

---
+++ Data Structures/Sqrt Decomposition.cpp
@@ -0,0 +1,23 @@
+void build(){
+    for(int i=0; i<n; i++){
+        block[i/len]+=v[i];
+    }
+}
+void upd(int id, int x){ //index em 0, logo upd(b-1, c)
+    block[id/len]-=v[id];
+    v[id]=x;
+    block[id/len]+=v[id];
+}
+ll query(int r){
+    ll ans=0;
+    for(int i=0; i<r/len; i++){
+        ans+=block[i];
+    }
+    for(int i=r/len*len; i<r; i++){
+        ans+=v[i];
+    }
+    return ans;
+}
+ll query(int l, int r){ //mesmo pelo index em 0, query(b, c), sem diminuir
+    return query(r)-query(l-1);
+}
