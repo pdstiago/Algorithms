85a751072d60e1e4f7f8e0c873c34263731190b7
Tiago Silva <tiago.pds.silva@gmail.com>
2023-03-21 03:53:41 -0300

    Update Sqrt Decomposition.cpp

--- Data Structures/Sqrt Decomposition.cpp
+++ Data Structures/Sqrt Decomposition.cpp
@@ -8,16 +8,23 @@ void upd(int id, int x){ //index em 0, logo upd(b-1, c)
     v[id]=x;
     block[id/len]+=v[id];
 }
-int query(int r){
+int query(int l, int r){ //index em 0, logo query(b-1, c-1)
     int ans=0;
-    for(int i=0; i<r/len; i++){
-        ans+=block[i];
-    }
-    for(int i=r/len*len; i<r; i++){
-        ans+=v[i];
+    int cl=l/len, cr=r/len;
+    if(cl==cr){
+        for(int i=l; i<=r; i++){
+            ans+=v[i];
+        }
+    }else{
+        for(int i=l; i<=(cl+1)*len-1; i++){
+            ans+=v[i];
+        }
+        for(int i=cl+1; i<=cr-1; i++){
+            ans+=block[i];
+        }
+        for(int i=cr*len; i<=r; i++){
+            ans+=v[i];
+        }
     }
     return ans;
-}
-int query(int l, int r){ //mesmo pelo index em 0, query(b, c), sem diminuir
-    return query(r)-query(l-1);
 }
