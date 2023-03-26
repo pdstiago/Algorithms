cb164e7bad3059ae044e50febf6c688f446d7b53
Tiago Silva <tiago.pds.silva@gmail.com>
2023-03-26 01:58:17 -0300

    Update Prime Factorization.cpp

--- Math/Prime Factorization.cpp
+++ Math/Prime Factorization.cpp
@@ -1,15 +1,41 @@
-vector<int> fatoracao(int n){
-    vector<int> fatores;
-    while(!(n&1)){
-        fatores.push_back(2);
-        n>>=1;
+ull modmul(ull a, ull b, ull M){
+    ll ret = a * b - M * ull(1.L / M * a * b);
+    return ret + M * (ret < 0) - M * (ret >= (ll)M);
+}
+ull modpow(ull b, ull e, ull mod){
+    ull ans = 1;
+    for (; e; b = modmul(b, b, mod), e /= 2)
+    if (e & 1) ans = modmul(ans, b, mod);
+    return ans;
+}
+ull pollard(ull n){ // O(N^(1/4))
+    auto f = [n](ull x, ull k) { return modmul(x, x, n) + k; };
+    ull x = 0, y = 0, t = 30, prd = 2, i = 1, q;
+    while (t++ % 40 || gcd(prd, n) == 1) {
+        if (x == y) x = ++i, y = f(x, i);
+        if ((q = modmul(prd, max(x,y) - min(x,y), n))) prd = q;
+        x = f(x, i), y = f(f(y, i), i);
     }
-    for(int i=3; i*i<=n; i+=2){
-        while(n%i==0){
-            fatores.push_back(i);
-            n/=i;
-        }
+    return gcd(prd, n);
+}
+bool isPrime(ull n){ // O(logN)
+    if (n < 2 || n % 6 % 4 != 1) return (n | 1) == 3;
+    vector<ull> A = {2, 325, 9375, 28178, 450775, 9780504,
+    1795265022};
+    ull s = __builtin_ctzll(n-1), d = n >> s;
+    for(ull a : A) {
+        ull p = modpow(a % n, d, n), i = s;
+        while (p != 1 && p != n - 1 && a % n && i--)
+        p = modmul(p, p, n);
+        if (p != n-1 && i != s) return 0;
     }
-    if(n!=1) fatores.push_back(n);
-    return fatores;
+    return 1;
+}
+vector<ull> factor(ull n){
+    if (n == 1) return {};
+    if (isPrime(n)) return {n};
+    ull x = pollard(n);
+    auto l = factor(x), r = factor(n / x);
+    l.insert(l.end(), r.begin(), r.end());
+    return l;
 }
