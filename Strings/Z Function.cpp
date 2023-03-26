1f4b2272f77efcfd673de43a5b016b0fa7bb297a
Tiago Silva <tiago.pds.silva@gmail.com>
2023-03-26 01:57:35 -0300

    Update and rename Z Algorithm.cpp to Z Function.cpp

--- Strings/Z Function.cpp
+++ Strings/Z Function.cpp
@@ -1,7 +1,7 @@
 //z é um vector de int que relaciona cada posição da string com um número 
 //que indica o tamanho da subsequência que começa nessa posição e coincide 
 //com o prefixo da string
-void z_algo(string s){
+void z_func(string s){
     for(int i=1, l=0, r=0; i<n; i++){
         if(i<=r){
             z[i]=min(r-i+1, z[i-l]);
