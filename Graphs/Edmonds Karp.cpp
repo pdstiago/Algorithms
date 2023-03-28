2406f890de09539d232ab7d7564e3b2a190ce5cb
Tiago Silva <tiago.pds.silva@gmail.com>
2023-03-28 19:52:27 -0300

    Update Edmonds Karp.cpp

--- Graphs/Edmonds Karp.cpp
+++ Graphs/Edmonds Karp.cpp
@@ -1,55 +1,60 @@
-int bfs(int s, int t){
-    for(int i=1; i<=n; i++) pai[i]={-1, -1};
-    
-    pai[s]={0, 0};
-    
-    queue<pii> fila;
-    fila.push({s, INF});
+struct Karp{ // Karp karp(n, m, 1, n); O(VE²)
+    vector<vector<pii> > vec;
+    vector<ll> cap;
+    vector<pii> pai;
+    int n, m=0, s, t;
 
-    while(!fila.empty()){
-        pii atual=fila.front();
-        fila.pop();
+    Karp(int n, int mf, int s, int t) : n(n), s(s), t(t){
+        vec.resize(n+1);
+        pai.resize(n+1);
+        cap.resize(2*mf);
+    }
 
-        for(pii i:vec[atual.f]){
-            if(pai[i.f].f==-1 && cap[i.s]){
-                pai[i.f]={atual.f, i.s};
+    void add_edge(int v, int u, ll x){
+        vec[v].pb({u, m});
+        vec[u].pb({v, m+1});
+        cap[m]=x;
+        cap[m+1]=0;
+        m+=2;
+    }
 
-                ll flow=min(atual.s, cap[i.s]);
+    int bfs(){
+        for(int i=1; i<=n; i++) pai[i]={-1, -1};
+        pai[s]={0, 0};
 
-                if(i.f==t) return flow;
+        queue<pair<int, ll> > fila;
+        fila.push({s, INFL});
 
-                fila.push({i.f, flow});
+        while(!fila.empty()){
+            auto atual=fila.front();
+            fila.pop();
+            for(pii i:vec[atual.f]){
+                if(pai[i.f].f==-1 && cap[i.s]){
+                    pai[i.f]={atual.f, i.s};
+                    ll flow=min(atual.s, cap[i.s]);
+                    if(i.f==t) return flow;
+                    fila.push({i.f, flow});
+                }
             }
         }
+        return 0;
     }
-    return 0;
-}
-ll karp(int s, int t){
-    ll flow=0, novo;
-    while((novo=bfs(s, t))){
-        flow+=novo;
-        int u=t;
-        while(u!=s){
-            int prev=pai[u].f, edge=pai[u].s;
 
-            ll atual=novo;
-
-            if(edge&1) atual*=-1, edge--;
-
-            cap[edge]-=atual, cap[edge+1]+=atual;
-
-            u=prev; 
+    ll max_flow(){
+        ll flow=0, atual;
+        
+        while((atual=bfs())){
+            flow+=atual;
+            int u=t;
+            vector<int> agr;
+            while(u!=s){
+                int prev=pai[u].f, edge=pai[u].s;
+                ll agr=atual;
+                if(edge&1) agr*=-1, edge--;
+                cap[edge]-=agr, cap[edge+1]+=agr;
+                u=prev; 
+            }
         }
+        return flow;
     }
-    return flow;
-}
-void input(){
-    for(int i=0; i<m; i++){
-        int a, b, c;
-        cin >> a >> b >> c;
-        vec[a].pb({b, 2*i});
-        vec[b].pb({a, 2*i+1});
-        cap[2*i]=c;
-        cap[2*i+1]=0;
-    }
-}
+};
