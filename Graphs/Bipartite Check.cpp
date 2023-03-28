e4f58726600d6115550e504574d6534623b13c68
Tiago Silva <tiago.pds.silva@gmail.com>
2023-03-28 20:36:22 -0300

    Update Bipartite Check.cpp

--- Graphs/Bipartite Check.cpp
+++ Graphs/Bipartite Check.cpp
@@ -1,16 +1,12 @@
-void dfs(int x){
+int check(int x){
     for(int i:vec[x]){
         if(!cor[i]){
-            if(cor[x]==1){
-                cor[i]=2;
-            }else{
-                cor[i]=1;
-            }
-            dfs(i);
+            if(cor[x]==1) cor[i]=2;
+            else cor[i]=1;
+            check(i);
         }else{
-            if(cor[x]==cor[i]){
-                check=1;
-            }
+            if(cor[x]==cor[i]) return 0;
         }
     }
+    return 1;
 }
