a2c54f5a12709226d5f63797a348440bc1a354c1
Tiago Silva <tiago.pds.silva@gmail.com>
2023-03-30 02:20:17 -0300

    Update Kosaraju.cpp

--- Graphs/Kosaraju.cpp
+++ Graphs/Kosaraju.cpp
@@ -3,23 +3,23 @@ void dfs(int x){
     for(int i:vec[x]){
         if(!memo[i]) dfs(i);
     }
-    st.pb(x);
+    order.pb(x);
 }
 void dfs2(int x){
     memo[x]=0;
-    for(int i:vec2[x]){
+    for(int i:rvec[x]){
         if(memo[i]) dfs2(i);
     }
-    st2.pb(x);
 }
-void kosaraju(){
+void scc(){
     int comp=0;
     for(int i=1; i<=n; i++){
         if(!memo[i]) dfs(i);
     }
-    for(int i=n-1; i>=0; i--){
-        if(memo[st[i]]){
-            dfs2(st[i]);
+    reverse(order.begin(), order.end());
+    for(int i:order){
+        if(memo[i]){
+            dfs2(i);
             comp++;
         }
     }
