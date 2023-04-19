0846214bbf2f4e72d6271946ef3b6454dbf096b4
Tiago Silva <tiago.pds.silva@gmail.com>
2023-04-18 23:05:06 -0300

    Update Hashing.cpp

--- Strings/Hashing.cpp
+++ Strings/Hashing.cpp
@@ -3,7 +3,7 @@ struct Hashing{
     int m=1004669333;
     int p=33; //se tiver maiúscula, 53 pode ser uma boa opção
     int inv_p=121778101;
-    vector<ll> p_pow, inv_pow, h;
+    vector<ll> p_pow, inv_pow, h, inv_h;
     string s;
 
     Hashing(string s) : s(s){
@@ -11,6 +11,7 @@ struct Hashing{
         p_pow.resize(n+1);
         inv_pow.resize(n+1);
         h.resize(n+1);
+        inv_h.resize(n+1);
     }
 
     void build(){
@@ -22,10 +23,15 @@ struct Hashing{
         }
         for(int i=1; i<=n; i++){
             h[i]=(h[i-1]+(s[i-1]-'a'+1)*p_pow[i-1])%m;
+            inv_h[i]=(inv_h[i-1]+(s[i-1]-'a'+1)*inv_pow[i-1])%m;
         }
     }
 
-    int range(int l, int r){ //index em 0, mesmo h sendo em 1
+    int range(int l, int r){ //index em 0
         return ((h[r+1]-h[l]+m)*inv_pow[l])%m;
+    }
+
+    int inv_range(int l, int r){ //index em 0
+        return ((inv_h[r+1]-inv_h[l]+m)*p_pow[l])%m;
     }
 };
