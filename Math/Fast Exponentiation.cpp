c6345378039195c08f174c93b22c16778e470f6f
Tiago Silva <tiago.pds.silva@gmail.com>
2023-05-08 23:07:13 -0300

    Update Fast Exponentiation.cpp

--- Math/Fast Exponentiation.cpp
+++ Math/Fast Exponentiation.cpp
@@ -1,12 +1,13 @@
-ll exp(ll b, ll e){ // O(logE)
+int mod(ll a){
+    return (a%MOD + MOD)%MOD;
+}
+ll exp(ll b, ll e){
     ll res=1;
     while(e){
         if(e&1){
-            res=mod(res*b, MOD);
-            res=mod(res, MOD);
+            res=mod(res*b);
         }
-        b=mod(b*b, MOD);
-        b=mod(b, MOD);
+        b=mod(b*b);
         e>>=1;
     }
     return res;
