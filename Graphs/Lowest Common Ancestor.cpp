200188158c5fc9681bd80789c11867692d25af7c
Tiago Silva <tiago.pds.silva@gmail.com>
2023-07-17 23:22:20 -0300

    Update Lowest Common Ancestor.cpp

--- Graphs/Lowest Common Ancestor.cpp
+++ Graphs/Lowest Common Ancestor.cpp
@@ -11,22 +11,19 @@ void dfs(int x, int p){
         dfs(i, x);
     }
 }
-pii lca(int a, int b){
+int lca(int a, int b){
     if(lv[a]<lv[b]) swap(a, b);
-    int dist=0;
     for(int i=18; i>=0; i--){
         if(lv[a]-(1<<i)>=lv[b]){
-            dist+=(1<<i);
             a=anc[a][i];
         }
     }
-    if(a==b) return {a, dist};
+    if(a==b) return a;
     for(int i=18; i>=0; i--){
         if(anc[a][i]!=-1 && anc[a][i]!=anc[b][i]){
             a=anc[a][i], b=anc[b][i];
-            dist+=2*(1<<i);
         }
     }
-    dist+=2;
-    return {anc[a][0], dist};
+    // dist = lv[a]+lv[b]-2*lv[lca];
+    return anc[a][0];
 }
