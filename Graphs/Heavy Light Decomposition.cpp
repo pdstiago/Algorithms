c2632c5d7ccd010e49643e8187ff61e21239664f
Tiago Silva <tiago.pds.silva@gmail.com>
2023-07-17 23:07:51 -0300

    Update Heavy Light Decomposition.cpp

--- Graphs/Heavy Light Decomposition.cpp
+++ Graphs/Heavy Light Decomposition.cpp
@@ -1,35 +1,18 @@
 struct HLD{
     int n, cont=0;
-    vector<int> sub, pai, heavy, head, pos, v, tree;
+    vector<int> sub, pai, heavy, chain, pos, v;
     vector<vector<int> > vec;
+    SEG seg;
 
     HLD(int n) : n(n){
+        seg.build(n);
+        v.resize(n+1);
         sub.resize(n+1);
         pai.resize(n+1);
         heavy.resize(n+1);
-        head.resize(n+1);
+        chain.resize(n+1);
         pos.resize(n+1);
-        v.resize(n+1);
-        tree.resize(4*n+1);
         vec.resize(n+1);
-    }
-
-    void upd(int node, int l, int r, int id, int val){
-        if(l==r){
-            tree[node]=val;
-            return;
-        }
-        int mid=(l+r)>>1;
-        if(l<=id && id<=mid) upd(2*node, l, mid, id, val);
-        else upd(2*node+1, mid+1, r, id, val);
-        tree[node]=min(tree[2*node], tree[2*node+1]);
-    }
-
-    int query(int node, int l, int r, int a, int b){
-        if(b<l || a>r) return INF;
-        if(l>=a && r<=b) return tree[node];
-        int mid=(l+r)>>1;
-        return min(query(2*node, l, mid, a, b), query(2*node+1, mid+1, r, a, b));
     }
 
     void add_edge(int a, int b){
@@ -37,25 +20,25 @@ struct HLD{
         vec[b].push_back(a);
     }
 
-    void dfs(int x=1, int p=0){
+    void dfs(int x, int p){
         sub[x]=1;
         int maior=0;
         for(int i:vec[x]){
             if(i==p) continue;
             dfs(i, x);
-            pai[i]=x;
             if(sub[i]>maior) maior=sub[i], heavy[x]=i;
+            pai[i]=x;
             sub[x]+=sub[i];
         }
     }
 
-    void hld(int x=1, int atual=1){
-        head[x]=atual;
+    void hld(int x, int atual){
+        chain[x]=atual;
         pos[x]=++cont;
-        upd(1, 1, n, cont, v[x]);
-        if(heavy[x]){
-            hld(heavy[x], atual);
-        }
+
+        seg.upd(1, 1, n, pos[x], pos[x], v[x]);
+
+        if(heavy[x]) hld(heavy[x], atual);
         for(int i:vec[x]){
             if(i==heavy[x] || i==pai[x]) continue;
             hld(i, i);
@@ -63,20 +46,53 @@ struct HLD{
     }
 
     void build(){
-        dfs();
-        hld();
+        dfs(1, 0);
+        hld(1, 1);
     }
     
-    int query_hld(int a, int b){
-        int ans=INF;
-        while(head[a]!=head[b]){
-            if(pos[head[a]]<pos[head[b]]) swap(a, b);
-            ans=min(ans, query(1, 1, n, pos[head[a]], pos[a]));
-            a=pai[head[a]];
+    int query_path(int a, int b){
+        int ans=0;
+
+        while(chain[a]!=chain[b]){
+            if(pos[chain[a]]<pos[chain[b]]) swap(a, b);
+
+            ans=seg.join(ans, seg.query(1, 1, n, pos[chain[a]], pos[a]));
+            a=pai[chain[a]];
         }
         if(pos[a]>pos[b]) swap(a, b);
+
         //if the values are on the edges pos[a]+1
-        ans=min(ans, query(1, 1, n, pos[a], pos[b]));
+        ans=seg.join(ans, seg.query(1, 1, n, pos[a], pos[b]));
         return ans;
+    }
+
+    void upd_path(int a, int b, int x){
+        while(chain[a]!=chain[b]){
+            if(pos[chain[a]]<pos[chain[b]]) swap(a, b);
+
+            seg.upd(1, 1, n, pos[chain[a]], pos[a], x);
+
+            a=pai[chain[a]];
+        }
+        if(pos[a]>pos[b]) swap(a, b);
+
+        //if the values are on the edges pos[a]+1
+        seg.upd(1, 1, n, pos[a], pos[b], x);
+    }
+
+    int query_sub(int a){
+        return seg.query(1, 1, n, pos[a], pos[a]+sub[a]-1);
+    }
+
+    void upd_sub(int a, int x){
+        seg.upd(1, 1, n, pos[a], pos[a]+sub[a]-1, x);
+    }
+
+    int query_node(int a){
+        return seg.query(1, 1, n, pos[a], pos[a]);
+    }
+
+    void upd_node(int a, int x){
+        seg.upd(1, 1, n, pos[a], pos[a], x);
     }
 };
