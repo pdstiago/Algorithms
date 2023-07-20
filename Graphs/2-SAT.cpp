f355a1ba11cdbdcc8d499c05ed5dce49637cc258
Tiago Silva <tiago.pds.silva@gmail.com>
2023-07-19 21:34:26 -0300

    Update 2-SAT.cpp

--- Graphs/2-SAT.cpp
+++ Graphs/2-SAT.cpp
@@ -1,51 +1,42 @@
-void dfs(int x){
-    memo[x]=1;
-    for(int i:vec[x]){
-        if(!memo[i]) dfs(i);
-    }
-    st.pb(x);
-}
-void dfs2(int x, int c){
-    comp[x]=c;
-    for(int i:vec2[x]){
-        if(!comp[i]) dfs2(i, c);
-    }
-}
-void add(int x, int sx, int y, int sy){
+//use kosaraju struct
+// i for '+' / i+m for '-'
+void add_edge(int x, int sx, int y, int sy){
+    // int a, b, c1, c2;
+    // char sa, sb;
+    // cin >> sa >> a >> sb >> b;
+
+    // (+ 2 - 1) quero topping 2, mas não quero o 1
+    // f = (x1 ou y1) e ... (xn ou yn), se f == 1, então é possível
+    // (x1 ou y1) = x1 -> ~y1 = y1 -> ~x1
+    // se x1 e ~x1 estiverem na mesma scc é impossível
+
+    // c1=(sa=='+'?1:0);
+    // c2=(sb=='+'?1:0);
+
+    // kosa.add_edge(a, c1, b, c2);
+    
     sx^=1, sy^=1;
     
-    vec[x+m*sx].pb(y+m*(!sy));
-    vec[y+m*sy].pb(x+m*(!sx));
+    vec[x+m*sx].push_back(y+m*(!sy));
+    vec[y+m*sy].push_back(x+m*(!sx));
  
-    vec2[y+m*(!sy)].pb(x+m*sx);
-    vec2[x+m*(!sx)].pb(y+m*sy);
+    rvec[y+m*(!sy)].push_back(x+m*sx);
+    rvec[x+m*(!sx)].push_back(y+m*sy);
 }
-int sat(){
-    for(int i=1; i<=n; i++){
-        int a, b, c1, c2;
-        char sa, sb;
-        cin >> sa >> a >> sb >> b;
-        // (+ 2 - 1) quero topping 2, mas nn quero o 1
-        // f = (x1 ou y1) e ... (xn ou yn), se f == 1, então é possível
-        // (x1 ou y1) = x1 -> ~y1 = y1 -> ~x1
-        // se x1 e ~x1 estiverem na mesma scc é impossível
-        c1=(sa=='+'?1:0);
-        c2=(sb=='+'?1:0);
+void build(){
+    //use normal build of kosaraju to find the components, remeber number of nodes is 2*m
     
-        add(a, c1, b, c2);
-    }
-    for(int i=1; i<=2*m; i++){
-        if(!memo[i]) dfs(i);
-    }
-    int cont=0;
-    for(int i=2*m-1; i>=0; i--){
-        if(!comp[st[i]]){
-            dfs2(st[i], ++cont);
-        }
-    }
+    int can=1;
     for(int i=1; i<=m; i++){
-        if(comp[i]==comp[i+m]) return 0;
-        resp[i]=comp[i]>comp[i+m]?'-':'+';
+        if(comp[i]==comp[i+m]) can=0;
+        resp[i]=comp[i]<comp[i+m]?'+':'-';
     }
-    return 1;
+    if(can){
+        cout << "YES" << endl;
+        for(int i=1; i<=m; i++){
+            cout << resp[i] << " ";
+        }
+    }else{
+        cout << "NO" << endl;
+    }
 }
