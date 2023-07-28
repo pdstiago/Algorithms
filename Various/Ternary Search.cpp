0e03f3b308a03b891c1bdaee85896058ad055ff4
Tiago Silva <tiago.pds.silva@gmail.com>
2023-07-28 18:20:29 -0300

    Create Ternary Search.cpp

---
+++ Various/Ternary Search.cpp
@@ -0,0 +1,30 @@
+int ternary(int l, int r){
+    while(r-l>=5){    
+        int mid=(l+r)>>1;
+        if(f(mid)<f(mid+1)){
+            l=mid;
+        }else{
+            r=mid+1;
+        }
+    }
+    for(int i=l+1; i<=r; i++){
+        if(f(l)<f(i)) l=i;
+    }
+    return l;
+}
+
+double ternary(double l, double r){
+    double eps=1e-9;
+    while(r-l>eps){
+        double m1=l+(r-l)/3;
+        double m2=r-(r-l)/3;
+        double f1=f(m1);
+        double f2=f(m2);
+        if(f1<f2){
+            l=m1;
+        }else{
+            r=m2;
+        }
+    }
+    return l;
+}
