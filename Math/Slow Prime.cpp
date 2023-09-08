f56cf1f7603257e1929e22104c3d7f339ddc4abf
Tiago Silva <tiago.pds.silva@gmail.com>
2023-09-07 22:10:45 -0300

    Create Slow Prime.cpp

---
+++ Math/Slow Prime.cpp
@@ -0,0 +1,15 @@
+vector<int> fact(int x){ //O(sqrt(N))
+    vector<int> factors;
+
+    if(x%2==0) factors.pb(2);
+    while(x%2==0) x>>=1;
+
+    for(int i=3; i*i<=x; i+=2){
+        if(x%i==0) factors.pb(i);
+        while(x%i==0) x/=i;
+    }
+
+    if(x>2) factors.pb(x);
+
+    return factors;
+}
