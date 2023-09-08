4bcd379e333ffe76ceb9a49a78d96705854dd69e
Tiago Silva <tiago.pds.silva@gmail.com>
2023-09-08 17:48:12 -0300

    Update Point.cpp

--- Geometry/Point.cpp
+++ Geometry/Point.cpp
@@ -2,7 +2,7 @@ template <class T> int sgn(T x) { return (x > 0) - (x < 0); }
 
 template<class T> struct Point{
 	typedef Point P;
-	T x, y, id;
+	T x, y;
 	explicit Point(T x=0, T y=0) : x(x), y(y) {}
 	bool operator<(P p) const { return tie(x,y) < tie(p.x,p.y); }
 	bool operator==(P p) const { return tie(x,y)==tie(p.x,p.y); }
