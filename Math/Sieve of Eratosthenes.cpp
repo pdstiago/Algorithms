7a4827adf0cef146ea7a187369819c014f138acf
Tiago Silva <tiago.pds.silva@gmail.com>
2023-09-07 21:52:13 -0300

    Update Sieve of Eratosthenes.cpp

--- Math/Sieve of Eratosthenes.cpp
+++ Math/Sieve of Eratosthenes.cpp
@@ -1,4 +1,4 @@
-void crivo(){ // O(N(log(logN)))
+vector<int> crivo(){ // O(N(log(logN)))
     n_primo[1]=1;
     for(int i=2; i<=n; i++){
         if(n_primo[i]) continue;
@@ -6,4 +6,9 @@ void crivo(){ // O(N(log(logN)))
             n_primo[i*j]=1;
         }
     }
+    vector<int> primes;
+    for(int i=2; i<=n; i++){
+        if(!n_primo[i]) primes.push_back(i); 
+    }
+    return primes;
 }
