6ba585b42eacdecd2450346d04357386417a97b4
Tiago Silva <tiago.pds.silva@gmail.com>
2023-09-09 16:42:06 -0300

    Update Kruskal.cpp

--- Graphs/Kruskal.cpp
+++ Graphs/Kruskal.cpp
@@ -1,24 +1,10 @@
-int ff(int x){
-    if(pai[x]==x) return x;
-    return pai[x]=ff(pai[x]);
-}
-void uu(int x, int y){
-    x=ff(x), y=ff(y);
-    if(x==y) return;
-    if(peso[y]>peso[x]) swap(x, y);
-    pai[y]=x;
-    peso[x]+=peso[y];
-}
 void kruscal(){
-     for(int i=1; i<=n; i++){
-        pai[i]=i;
-        peso[i]=1;
-    }
-    sort(all(total));
-    for(pip i:total){
-        if(ff(i.s.f)!=ff(i.s.s)){
-            uu(i.s.f, i.s.s);
-            resp+=i.f;
+    init();
+    sort(total.begin(), total.end());
+    for(array<int, 3> i:total){
+        if(ff(i[1])!=ff(i[2])){
+            uu(i[1], i[2]);
+            resp+=i[0];
         }
     }
 }
