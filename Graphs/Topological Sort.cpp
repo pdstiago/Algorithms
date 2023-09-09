9e5464f3728d9b1f68bd2c45aabef59967daf574
Tiago Silva <tiago.pds.silva@gmail.com>
2023-09-09 16:49:55 -0300

    Update Topological Sort.cpp

--- Graphs/Topological Sort.cpp
+++ Graphs/Topological Sort.cpp
@@ -1,16 +1,21 @@
-void top_sort(){
+vector<int> top_sort(){
+    vector<int> resp;
     queue<int> fila;
     for(int i=1; i<=n; i++){
         if(!grau[i]) fila.push(i);
     }
     while(!fila.empty()){
         int u=fila.front();
-        resp.pb(u);
+        resp.push_back(u);
         fila.pop();
         for(int i:vec[u]){
             grau[i]--;
             if(!grau[i]) fila.push(i);
         }
     }
-    if(sz(resp)<n) cout << "IMPOSSIBLE" << endl;
+    if((int)resp.size()<n){
+        return {};
+    }else{
+        return resp;
+    }
 }
