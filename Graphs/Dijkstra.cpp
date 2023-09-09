94c7f9cf13605afef9089f795ca45e1bc47d522f
Tiago Silva <tiago.pds.silva@gmail.com>
2023-09-09 17:27:04 -0300

    Update Dijkstra.cpp

--- Graphs/Dijkstra.cpp
+++ Graphs/Dijkstra.cpp
@@ -1,18 +1,19 @@
 void dj(int x){
-    memset(dist, INF, sizeof(dist));
-    memset(memo, 0, sizeof(memo));
-    priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > fila;
+    for(int i=1; i<=n; i++) dist[i]=INF;
+    priority_queue<pair<int, int> > fila;
     dist[x]=0;
     fila.push({0, x});
+
     while(!fila.empty()){
-        int u=fila.top().second;
+        int d = -fila.top().first, u = fila.top().second;
         fila.pop();
-        if(memo[u]) continue;
-        memo[u]=1;
+        
+        if(d>dist[u]) continue;
+
         for(pair<int, int> i:vec[u]){
             if(dist[i.first]>dist[u]+i.second){
                 dist[i.first]=dist[u]+i.second;
-                fila.push({dist[i.first], i.first});
+                fila.push({-dist[i.first], i.first});
             }
         }
     }
