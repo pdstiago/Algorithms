ac19764ad7f01f048191783fb6736d17e3a09d8a
Tiago Silva <tiago.pds.silva@gmail.com>
2023-09-09 17:27:33 -0300

    Update Dijkstra.cpp

--- Graphs/Dijkstra.cpp
+++ Graphs/Dijkstra.cpp
@@ -3,13 +3,10 @@ void dj(int x){
     priority_queue<pair<int, int> > fila;
     dist[x]=0;
     fila.push({0, x});
-
     while(!fila.empty()){
         int d = -fila.top().first, u = fila.top().second;
         fila.pop();
-        
         if(d>dist[u]) continue;
-
         for(pair<int, int> i:vec[u]){
             if(dist[i.first]>dist[u]+i.second){
                 dist[i.first]=dist[u]+i.second;
