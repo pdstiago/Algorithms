2852a104455146bc6d77dc999baebbb0a216e198
Tiago Silva <tiago.pds.silva@gmail.com>
2023-09-10 18:07:24 -0300

    Update Implicit Persistent Segment Tree.cpp

--- Data Structures/Implicit Persistent Segment Tree.cpp
+++ Data Structures/Implicit Persistent Segment Tree.cpp
@@ -1,75 +1,86 @@
-template<class T> struct SegP{
+struct SegP{
     int n, cont=0, t=0;
-    vector<T> tree, lazy;
+    vector<int> tree, lazy;
     vector<int> esq, dir, ver;
 
-    SegP(int n, int q) : n(n), tree(3*(n+1)+18*(q+1)), esq(3*(n+1)+18*(q+1)), dir(3*(n+1)+18*(q+1)), 
-    lazy(3*(n+1)+18*(q+1)), ver(q+1) {}
+    SegP(int n, int q) : n(n), tree(2*(n+1)+4*18*(q+1)), esq(5*(n+1)+4*18*(q+1)), dir(2*(n+1)+4*18*(q+1)), 
+    lazy(2*(n+1)+4*18*(q+1)), ver(q+2) {}
 
-    T join(T a, T b){
+    int join(int a, int b){
         return a+b;
     }
 
-    void build(int node, int l, int r, vector<int> &v){
-        if(l==r){
-            tree[node]=v[l];
-            return;
-        }
+    int newleaf(int val){
+        int node = ++cont;
+        esq[node] = dir[node] = 0;
+        tree[node] = val;
+        return node;
+    }
+
+    int newpai(int l, int r){
+        int node = ++cont;
+        esq[node] = l, dir[node] = r;
+        tree[node] = join(tree[l], tree[r]);
+        return node;
+    }
+
+    int build(int l, int r, vector<int> &v){
+        if(l==r) return newleaf(v[l]);
         int mid = (l+r)>>1;
-        esq[node]=++cont, dir[node]=++cont;
-        build(esq[node], l, mid, v);
-        build(dir[node], mid+1, r, v);
-        tree[node]=join(tree[esq[node]], tree[dir[node]]);
+        return newpai(build(l, mid, v), build(mid+1, r, v));
     }
 
     void build(vector<int> &v){
-        build(ver[++t]=++cont, 1, n, v);
+        ver[t] = build(1, n, v);
     }
 
-    T query(int node, int l, int r, int a, int b){
-        if(a>r || b<l) return 0; //remember to change
-        if(a<=l && b>=r) return tree[node]+lazy[node];
-        int mid=(l+r)>>1;
-        return lazy[node] + join(query(esq[node], l, mid, a, b), query(dir[node], mid+1, r, a, b));
+    int newlazy(int node, int l, int r, int val){
+        int no = ++cont;
+        esq[no] = esq[node], dir[no] = dir[node];
+        lazy[no] = lazy[node];
+        lazy[no]+=val;
+        tree[no] = tree[node]+(r-l+1)*val;
+        return no;
     }
 
-    T query(int tt, int a, int b){
-        return query(ver[tt], 1, n, a, b);
-    }
-
-    void upd(int lnode, int node, int l, int r, int pos, int val){
-        if(l==r){
-            tree[node]=val;
-            return;
+    void unlazy(int node, int l, int r){
+        if(lazy[node]){
+            if(l!=r){
+                int mid=(l+r)>>1;
+                esq[node] = newlazy(esq[node], l, mid, lazy[node]);
+                dir[node] = newlazy(dir[node], mid+1, r, lazy[node]);
+            }
+            lazy[node]=0;
         }
+    }
+
+    int upd(int node, int l, int r, int pos, int val){
+        if(l==r) return newleaf(val);
         int mid=(l+r)>>1;
         if(pos<=mid){
-            upd(esq[lnode], esq[node]=++cont, l, mid, pos, val);
-            tree[node] = join(tree[esq[node]], tree[dir[node]=dir[lnode]]);
-        }else{
-            upd(dir[lnode], dir[node]=++cont, mid+1, r, pos, val);
-            tree[node] = join(tree[esq[node]=esq[lnode]], tree[dir[node]]);
+            return newpai(upd(esq[node], l, mid, pos, val), dir[node]);
         }
+        return newpai(esq[node], upd(dir[node], mid+1, r, pos, val));
     }
 
-    void upd(int lnode, int node, int l, int r, int a, int b, int val){
-        tie(tree[node], lazy[node], esq[node], dir[node]) = {tree[lnode], lazy[lnode], esq[lnode], dir[lnode]};
-
-        if(a>r || b<l) return;
-
-        if(a<=l && b>=r){
-            lazy[node]+=val;
-            return;
-        }
-
+    int upd(int node, int l, int r, int a, int b, int val){
+        if(a>r || b<l) return node;
+        if(a<=l && b>=r) return newlazy(node, l, r, val);
+        unlazy(node, l, r);
         int mid=(l+r)>>1;
+        return newpai(upd(esq[node], l, mid, a, b, val), upd(dir[node], mid+1, r, a, b, val));
+    }
 
-        upd(esq[lnode], esq[node]=++cont, l, mid, a, b, val);
-        upd(dir[lnode], dir[node]=++cont, mid+1, r, a, b, val);
+    int query(int node, int l, int r, int a, int b){
+        if(a>r || b<l) return 0; //remember to change
+        if(a<=l && b>=r) return tree[node];
+        unlazy(node, l, r);
+        int mid=(l+r)>>1;
+        return join(query(esq[node], l, mid, a, b), query(dir[node], mid+1, r, a, b));
+    }
 
-        tree[node] = join(tree[esq[node]], tree[dir[node]]);
-
-        lazy[node] = lazy[lnode];
+    int query(int tt, int a, int b){
+        return query(ver[tt], 1, n, a, b);
     }
 
     void copy(int tt){
@@ -77,22 +88,18 @@ template<class T> struct SegP{
     }
 
     void upd(int tt, int pos, int val){
-        int prox=++cont;
-        upd(ver[tt], prox, 1, n, pos, val);
-        ver[tt]=prox;
+        ver[tt] = upd(ver[tt], 1, n, pos, val);
     }
 
     void upd(int tt, int a, int b, int val){
-        int prox=++cont;
-        upd(ver[tt], prox, 1, n, a, b, val);
-        ver[tt]=prox;
+        ver[tt] = upd(ver[tt], 1, n, a, b, val);
     }
 
     void upd_copy(int tt, int pos, int val){
-        upd(ver[++t]=++cont, ver[tt], 1, n, pos, val);
+        ver[++t] = upd(ver[tt], 1, n, pos, val);
     }
 
     void upd_copy(int tt, int a, int b, int val){
-        upd(ver[++t]=++cont, ver[tt], 1, n, a, b, val);
+        ver[++t] = upd(ver[tt], 1, n, a, b, val);
     }
 };
