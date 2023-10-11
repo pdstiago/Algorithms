3b0fd7a60c199f8bd2c4d59627e76708ca2fbfde
Tiago Silva <tiago.pds.silva@gmail.com>
2023-10-11 19:52:00 -0300

    Update Digit DP.cpp

--- Various/Digit DP.cpp
+++ Various/Digit DP.cpp
@@ -1,1 +1,28 @@
-teste
+ll solve(string &s, int i, int tight, int last, int started){ //numbers that no two adjacent digits are the same
+    if(i==(int)s.size()) return 1;
+
+    if(!tight && dp[i][last][started]!=-1) return dp[i][last][started];
+
+    int lim=(tight?s[i]-'0':9);
+
+    ll resp=0;
+    for(int j=0; j<=lim; j++){
+        if(started && j==last) continue;
+        resp+=solve(s, i+1, tight&(j==lim), j, (started|j)>0);
+    }
+
+    if(!tight) return dp[i][last][started]=resp;
+    return resp;
+}
+
+ll func(ll a, ll b){
+    string agr1=to_string(a-1);
+    memset(dp, -1, sizeof(dp));
+    ll ans1 = solve(agr1, 0, 1, 10, 0);
+
+    string agr2=to_string(b);
+    memset(dp, -1, sizeof(dp));
+    ll ans2 = solve(agr2, 0, 1, 10, 0);
+    
+    return ans2-ans1;
+}
