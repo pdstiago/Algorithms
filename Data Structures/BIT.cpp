8300200865d925daceaebe89ad2f03fc9c72d659
Tiago Silva <tiago.pds.silva@gmail.com>
2023-10-11 19:03:47 -0300

    Update BIT.cpp

--- Data Structures/BIT.cpp
+++ Data Structures/BIT.cpp
@@ -7,3 +7,15 @@ int sum(int x){
     for(; x; x-=x&-x) ans+=bit[x];
     return ans;
 }
+
+int busca(int val){
+    int sum=0, pos=0;
+    for(int i=18; i>=0; i--){
+        int newpos = pos+(1<<i);
+        if(newpos<n && sum+bit[newpos]<val){
+            sum+=bit[newpos];
+            pos=newpos;
+        }
+    }
+    return pos+1;
+}
