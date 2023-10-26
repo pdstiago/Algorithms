97f930fd3c9fb6f765233d4dcb4a75e8fb8feca7
Tiago Silva <tiago.pds.silva@gmail.com>
2023-10-25 22:17:31 -0300

    Create Fast Lowest Common Ancestor.cpp

---
+++ Graphs/Fast Lowest Common Ancestor.cpp
@@ -0,0 +1,22 @@
+//sparse table of min on walk, with n as n-1
+
+void dfs(int x, int p){
+    tempo[x]=t++;
+    for(int i:vec[x]){
+        if(i==p) continue;
+        path.push_back(x);
+        walk.push_back(tempo[x]);
+        lv[i]=lv[x]+1;
+        dfs(i, x);
+    }
+}
+
+int lca(int a, int b){
+    if(a==b) return a;
+    tie(a, b) = minmax(tempo[a], tempo[b]);
+    return path[query(a, b)];
+}
+
+int dist(int a, int b){
+    return lv[a]+lv[b]-2*lv[lca(a, b)];
+}
