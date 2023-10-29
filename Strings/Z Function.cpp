1511feb12e04639b61b063a83a9c079b15d0c48d
Tiago Silva <tiago.pds.silva@gmail.com>
2023-10-29 18:56:58 -0300

    Update Z Function.cpp

--- Strings/Z Function.cpp
+++ Strings/Z Function.cpp
@@ -1,7 +1,9 @@
-//z é um vector de int que relaciona cada posição da string com um número 
-//que indica o tamanho da subsequência que começa nessa posição e coincide 
-//com o prefixo da string
-void z_func(string s){
+//Z relaciona cada posição i da string com um número que indica o tamanho da subsequência que começa nessa posição i e 
+//coincide com o prefixo da string.
+//Em outras palavras, Z[i] é o tamanho da maior string, que é prefixo de S e um prefixo do sufixo de S que começa na posição i.
+vector<int> build(string &s){ // O(N)
+    int n = (int)s.size();
+    vector<int> z(n);
     for(int i=1, l=0, r=0; i<n; i++){
         if(i<=r){
             z[i]=min(r-i+1, z[i-l]);
