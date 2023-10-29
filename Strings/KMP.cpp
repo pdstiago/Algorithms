fd73176f28758c3817fc8f86664771e4004e07b2
Tiago Silva <tiago.pds.silva@gmail.com>
2023-10-29 19:05:49 -0300

    Update KMP.cpp

--- Strings/KMP.cpp
+++ Strings/KMP.cpp
@@ -1,16 +1,11 @@
-// calcula prefix function, maior prefixo que também é sufixo até a string de 0 até i.
-vector<int> kmp(string s){
+// calcula Prefix Function, ou seja, pi[i] indica o tamanho do maior prefixo que também é sufixo da substring de 0 até i.
+vector<int> kmp(string &s){
     int n = (int)s.size();
-
     vector<int> pi(n);
-
     for(int i=1; i<n; i++){
         int j = pi[i-1];
-
         while(j>0 && s[i]!=s[j]) j=pi[j-1];
-
         if(s[i]==s[j]) j++;
-
         pi[i]=j;
     }
     return pi;
