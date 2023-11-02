ef6afd5942eb322e81903d4d4f966df3cb71b495
Tiago Silva <tiago.pds.silva@gmail.com>
2023-11-02 19:23:35 -0300

    Update Small to Large.cpp

--- Graphs/Small to Large.cpp
+++ Graphs/Small to Large.cpp
@@ -1,4 +1,4 @@
-int sub[mxn], tour[mxn], tin[mxn], tout[mxn], lv[mxn], cont[mxn], timer;
+int sub[mxn], tour[mxn], tin[mxn], tout[mxn], freq[mxn], timer;
 
 // dfs(1, 0) / large(1, 0, 1)
 
@@ -31,22 +31,22 @@ void large(int x, int p, bool ok){
         large(big, x, 1);
     }
     //realiza a operação para o vertice atual
-    cont[color[x]]++;
+    freq[color[x]]++;
     for(int i:vec[x]){
         if(i!=p && i!=big){
             for(int j=tin[i]; j<tout[i]; j++){
                 int u=tour[j];
-                cont[color[u]]++;
+                freq[color[u]]++;
             }
         }
     }
-    //cont[v] possui a quantidade de vertices da cor v na subarvore de x
+    //freq[v] possui a quantidade de vertices da cor v na subárvore de x
     
     // faço o inverso da operação
     if(!ok){
         for(int i=tin[x]; i<tout[x]; i++){
             int u=tour[i];
-            cont[color[u]]--;
+            freq[color[u]]--;
         }
     }
 }
