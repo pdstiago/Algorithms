cf8bd5cc18bd382ee2109b2a03904d094595d31d
pdstiago <tiago.pds.silva@gmail.com>
2023-12-29 19:52:06 -0300

    Update Weighted Matching.cpp

--- Graphs/Weighted Matching.cpp
+++ Graphs/Weighted Matching.cpp
@@ -1,13 +1,14 @@
+// Para matching mínimo, multiplicar os pesos por -1
 pair<int, vector<int> > hung(const vector<vector<int> > &a){
-    int n = (int)a.size() + 1, m = (int)a[0].size() + 1;
+    int n=(int)a.size()+1, m=(int)a[0].size()+1;
 
     vector<int> u(n), v(m), p(m), ans(n-1);
 
     for(int i=1; i<n; i++){
-        p[0] = i;
-        int j0 = 0;
+        p[0]=i;
+        int j0=0;
         vector<int> dist(m, INF), pre(m, -1);
-        vector<bool> done(m + 1);
+        vector<bool> done(m+1);
 
         do{ 
             done[j0]=true;
@@ -15,7 +16,7 @@ pair<int, vector<int> > hung(const vector<vector<int> > &a){
 
             for(int j=1; j<m; j++){
                 if(!done[j]){
-                    auto cur = a[i0-1][j-1] - u[i0] - v[j];
+                    auto cur=a[i0-1][j-1]-u[i0]-v[j];
 
                     if(cur<dist[j]) dist[j]=cur, pre[j]=j0;
                     
