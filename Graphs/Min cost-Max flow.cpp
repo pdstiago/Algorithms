4c886c3280cc669e5b93ec4034f76be8cb8d9f85
Tiago Silva <tiago.pds.silva@gmail.com>
2023-12-29 21:39:23 -0300

    Update Min cost-Max flow.cpp

--- Graphs/Min cost-Max flow.cpp
+++ Graphs/Min cost-Max flow.cpp
@@ -4,32 +4,32 @@ struct min_cost{
 	vector<int> ptr, nxt, zu, pari;
 	vector<ll> capa, cost, pot, dist, flows, slopes;
 	vector<bool> vis;
-
+	
 	min_cost(int n, int s, int t) : n(n), s(s), t(t), ptr(n+1, -1), dist(n+1), vis(n+1), pari(n+1) {}
-
+	
 	void add_edge(int u, int v, ll w, ll c){
 		nxt.push_back(ptr[u]); zu.push_back(v); capa.push_back(w); cost.push_back(c); ptr[u] = m++;
 		nxt.push_back(ptr[v]); zu.push_back(u); capa.push_back(0); cost.push_back(-c); ptr[v] = m++;
 	}
-
+	
 	void shortest(){
 		using E = pair<ll, int>;
 		priority_queue<E, vector<E>, greater<E> > fila;
-
+	
 		for(int i=1; i<=n; i++){
 			dist[i]=INFL, vis[i]=false;
 		}
-
+	
 		for(fila.emplace(dist[s]=0, s); !fila.empty();){
 			ll c=fila.top().first;
 			int u=fila.top().second; 
 			fila.pop();
-
+	
 			if(vis[u]) continue;
 			vis[u]=true;
-
+	
 			if(u==t) return;
-
+	
 			for(int i=ptr[u]; ~i; i=nxt[i]){
 				if(capa[i]>eps){
 					int v=zu[i];
@@ -42,12 +42,12 @@ struct min_cost{
 			}
 		}
 	}
-
+	
 	pair<ll, ll> run(ll limFlow=INFL){
 		pot.assign(n+1, 0);
 		flows={0};
 		slopes.clear();
-
+	
 		while(1){
 			bool upd=false;
 			for(int i=0; i<m; i++){
@@ -66,15 +66,15 @@ struct min_cost{
 			shortest(); 
 			ll f=limFlow-flow;
 			if(!vis[t]) break;
-
+	
 			for(int i=1; i<=n; i++) pot[i]+=min(dist[i], dist[t]);
-
+	
 			for(int v=t; v!=s;){ 
 				int i=pari[v];
 				if(f>capa[i]) f=capa[i];
 				v=zu[i^1];
 			}
-
+	
 			for(int v=t; v!=s;){
 				int i=pari[v];
 				capa[i]-=f, capa[i^1]+=f, v=zu[i^1];
