eeb57f7e5f46c96d0a9f83ee32f8bfdd3ff67f50
Tiago Silva <tiago.pds.silva@gmail.com>
2024-05-30 23:14:21 -0300

    Update Convex Hull.cpp

--- Geometry/Convex Hull.cpp
+++ Geometry/Convex Hull.cpp
@@ -1,28 +1,24 @@
-vector<P> ch(){
+vector<P> ch(vector<P> &v){
     sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+    if(v.size()<=1) return v;
 
-    vector<P> upper, lower, convex;
+    vector<P> up, lw, cv;
     
     // a condição do cross será < quando incluir colinear, senão <=
-    // #define sz(x) (int)x.size()
     
+    int n = (int)v.size();
     for(int i=0; i<n; i++){
-        while(sz(upper)>1 && v[i].cross(upper[sz(upper)-2], upper[sz(upper)-1])<0){
-            upper.pop_back();
-        }
-        upper.push_back(v[i]);
+        while(lw.size()>1 && v[i].cross(lw[lw.size()-2], lw[lw.size()-1])>=0) lw.pop_back();
+        lw.push_back(v[i]);
     }
     for(int i=n-1; i>=0; i--){
-        while(sz(lower)>1 && v[i].cross(lower[sz(lower)-2], lower[sz(lower)-1])<0){
-            lower.pop_back();
-        }
-        lower.push_back(v[i]);
+        while(up.size()>1 && v[i].cross(up[up.size()-2], up[up.size()-1])>=0) up.pop_back();
+        up.push_back(v[i]);
     }
-    for(int i=0; i<sz(upper)-1; i++){
-        convex.push_back(upper[i]);
-    }
-    for(int i=0; i<sz(lower)-1; i++){
-        convex.push_back(lower[i]);
-    }
-    return convex;
+    lw.pop_back();
+    up.pop_back();
+    cv = up;
+    for(P i:lw) cv.push_back(i);
+    return cv;
 }
