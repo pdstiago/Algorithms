62ad5e87c0bb1a520d459f5c8288110524c597df
pdstiago <tiago.pds.silva@gmail.com>
2024-08-28 13:49:34 -0300

    added template to Dinic.cpp

--- Graphs/Dinic.cpp
+++ Graphs/Dinic.cpp
@@ -1,23 +1,20 @@
-struct Edge{
-    int v, u;
-    ll cap, flow=0;
-    Edge(int v, int u, ll cap) : v(v), u(u), cap(cap) {}
-};
 // O(min(V*max_flow, V²*E))
 // Grafo com capacidades 1: O(min(M*sqrt(M), M*N^(2/3)))
 // Todo vértice tem grau de entrada ou saída 1: O(M*sqrt(N))
+template<typename T>
 struct Dinic{
-    int n, m=0;
+    struct Edge {int v, u; T cap, flow;};
+    int m=0;
     vector<Edge> edges;
     vector<vector<int> > vec;
     vector<int> lv, pos;
     queue<int> fila;
  
-    Dinic(int n) : n(n), vec(n+1), lv(n+1), pos(n+1) {}
+    Dinic(int n) : vec(n), lv(n), pos(n) {}
  
-    void add_edge(int v, int u, ll cap) {
-        edges.emplace_back(v, u, cap);
-        edges.emplace_back(u, v, 0);
+    void add_edge(int v, int u, T cap) {
+        edges.push_back({v, u, cap, 0});
+        edges.push_back({u, v, 0, 0});
         vec[v].push_back(m);
         vec[u].push_back(m+1);
         m+=2;
@@ -38,7 +35,7 @@ struct Dinic{
         return lv[t]!=-1;
     }
  
-    ll dfs(int v, int t, ll menor) {
+    T dfs(int v, int t, T menor) {
         if(!menor) return 0;
         if(v==t) return menor;
  
@@ -48,7 +45,7 @@ struct Dinic{
  
             if(lv[v]+1!=lv[u] || edges[i].cap-edges[i].flow<1) continue;
  
-            ll agr=dfs(u, t, min(menor, edges[i].cap-edges[i].flow));
+            T agr=dfs(u, t, min(menor, edges[i].cap-edges[i].flow));
             if(!agr) continue;
  
             edges[i].flow+=agr;
@@ -59,8 +56,8 @@ struct Dinic{
         return 0;
     }
  
-    ll max_flow(int s, int t){
-        ll flow=0;
+    T max_flow(int s, int t){
+        T flow=0;
         while(1){
             fill(lv.begin(), lv.end(), -1);
  
@@ -71,11 +68,11 @@ struct Dinic{
  
             fill(pos.begin(), pos.end(), 0);
  
-            while(ll atual=dfs(s, t, INFL)) flow+=atual;
+            while(T atual=dfs(s, t, INF)) flow+=atual; //remember to change INF
         }
         return flow;
     }
-    vector<pair<int, int> > recap(){
+    auto recap(){
         vector<pair<int, int> > resp;
         for(int i=0; i<(int)edges.size(); i+=2){
             if(lv[edges[i].v]>=0 && lv[edges[i].u]==-1) resp.push_back({edges[i].v, edges[i].u});
