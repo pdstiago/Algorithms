4f5f2149853e76ec08c50c734276e9839db4105b
pdstiago <tiago.pds.silva@gmail.com>
2024-08-29 23:03:11 -0300

    added template

--- Graphs/Lower_Bound Max Flow.cpp
+++ Graphs/Lower_Bound Max Flow.cpp
@@ -1,18 +1,18 @@
-struct LB_flow{
+template<class T> struct LB_flow{
     int n;
-    vector<ll> d;
-    Dinic dinic;
+    vector<T> d;
+    Dinic<T> dinic;
  
     LB_flow(int n) : n(n), dinic(n+2), d(n) {}
 
-    void add_edge(int a, int b, ll l, ll c){
+    void add_edge(int a, int b, T l, T c){
         d[a]-=l, d[b]+=l;
         dinic.add_edge(a, b, c-l);
     }
 
     bool has_flow(int s, int t){
-        dinic.add_edge(t, s, INFL);
-        ll sum = 0;
+        dinic.add_edge(t, s, INF); // remember to change INF
+        T sum = 0;
         for(int i=0; i<n; i++){
             if(d[i]>0){
                 sum+=d[i];
@@ -24,7 +24,7 @@ struct LB_flow{
         return (sum == dinic.max_flow(n, n+1));
     }
 
-    ll max_flow(int s, int t){
+    T max_flow(int s, int t){
         if(!has_flow(s, t)) return -1;
         return dinic.max_flow(s, t);
     }
