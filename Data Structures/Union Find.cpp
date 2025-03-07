b61c8160546b4fd051229c2232532bdf5c97ef88
pdstiago <tiago.pds.silva@gmail.com>
2025-03-07 18:40:36 -0300

    structify dsu

--- Data Structures/Union Find.cpp
+++ Data Structures/Union Find.cpp
@@ -1,21 +1,21 @@
-int pai[mxn], peso[mxn];
-
-int ff(int x){
-    if(pai[x]==x) return x;
-    return pai[x]=ff(pai[x]);
-}
-
-void uu(int x, int y){
-    x=ff(x), y=ff(y);
-    if(x==y) return;
-    if(peso[y]>peso[x]) swap(x, y);
-    pai[y]=x;
-    peso[x]+=peso[y];
-}
-
-void build(){
-    for(int i=1; i<=n; i++){
-        pai[i]=i;
-        peso[i]=1;
+struct DSU{
+    vector<int> pai, peso;
+    DSU(int n) : pai(n+1), peso(n+1) {
+        for(int i=1; i<=n; i++){
+            pai[i] = i;
+            peso[i] = 1;
+        }
     }
-}
+    int ff(int x){
+        if(pai[x]==x) return x;
+        return pai[x]=ff(pai[x]);
+    }
+    int uu(int x, int y){
+        x=ff(x), y=ff(y);
+        if(x==y) return 0;
+        if(peso[y]>peso[x]) swap(x, y);
+        pai[y]=x;
+        peso[x]+=peso[y];
+        return 1;
+    }
+};
